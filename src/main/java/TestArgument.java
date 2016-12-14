7581c7331c78571f462d79ee33bd467042e184b3
zhaoyibin <zhaoyibin@le.com>
2016-12-14 11:35:45 +0800

    more test

---
+++ src/main/java/TestArgument.java
@@ -0,0 +1,53 @@
+/**
+ * Created by Nico on 12/14/16 10:31.
+ */
+public class TestArgument {
+
+    static void increment(int i) {
+        i++;
+    }
+
+    static void testHandle(Gizmo gizmo) {
+//        gizmo = new Gizmo(2);
+        gizmo.setI(2);
+    }
+
+    public static void main(String[] args) {
+        int i = 1;
+        increment(i);
+        System.out.println(i);
+        Gizmo gizmo = new Gizmo(1);
+        testHandle(gizmo);
+        gizmo.print();
+    }
+}
+
+class Gizmo {
+    private int i;
+
+    public Gizmo(int i) {
+        this.i = i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void print() {
+        System.out.println(i);
+    }
+
+    final private void method() {}
+
+}
+
+class childGizmo extends Gizmo {
+
+    public childGizmo(int i) {
+        super(i);
+    }
+
+    void method() {
+
+    }
+}
